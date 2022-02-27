fa9cfa0eed775812fb4c4893c9a2224a78fe4bc9
MadhavBahl <madhavbahl10@gmail.com>
2022-02-27 13:39:03 +0530

    Add boilerplate

--- src/components/Currencies/CurrencyCard.jsx
+++ src/components/Currencies/CurrencyCard.jsx
@@ -1,35 +1,8 @@
 import React from 'react';
 import './Currency.css';
 
-const CurrencyCard = ({ currency, onCardClick, active, reachedLimit }) => {
-    const coinName = currency.name;
-    const coinSymbol = currency.symbol;
-    const coinPrice = currency.price_usd;
-
-    return (
-        <div
-            onClick={() => onCardClick(currency)}
-            className={`crypto-card ${active && 'crypto-card-active'} ${
-                reachedLimit ? 'not-hoverable' : 'hoverable'
-            }`}
-        >
-            <img
-                className='currency-logo'
-                src={`https://cryptoicon-api.vercel.app/api/icon/${coinSymbol.toLowerCase()}`}
-                alt='crypto-icon'
-            />
-            <div className='coin-name'>
-                <span className='coin-name-title'>{coinName}</span>
-                <span className='coin-symbol'>
-                    &nbsp;{coinSymbol.toUpperCase()}
-                </span>
-            </div>
-            <div className='coin-price'>
-                <span className='coin-dollar'>$</span>
-                <span className='coin-price-value'>{coinPrice}</span>
-            </div>
-        </div>
-    );
+const CurrencyCard = () => {
+    return <div className='crypto-card'></div>;
 };
 
 export default CurrencyCard;
--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -3,11 +3,7 @@ import logo from './../../assets/landing.png';
 import './Landing.css';
 
 const Landing = () => {
-    return (
-        <section>
-            <img className='landing-cover' src={logo} alt='landing-cover' />
-        </section>
-    );
+    return <section></section>;
 };
 
 export default Landing;
